fe7bb482337beec474a33c02c3ff577f1e495bda
John Ruiz <jar349@github.com>
2020-05-17 15:07:33 -0400

    fix responses

--- pyslackops/handlers.py
+++ pyslackops/handlers.py
@@ -93,7 +93,7 @@ class PBotHandler(NamespaceHandler):
         self.pbot = pbot
         self.log = logging.getLogger(__name__)
         self.cmd_map = {
-            "help": self.get_basic_help,
+            "help": self.get_help,
             "ping": self.ping,
             "list": self.list_namespaces,
             "test": self.test_handler
@@ -115,6 +115,10 @@ class PBotHandler(NamespaceHandler):
             "protocol_version": "0.1"
         }
 
+    def get_help(self, cmd_args):
+        # maybe one day support better help on individual commands.  For now, just get basic help
+        return self.get_basic_help()
+
     def get_basic_help(self):
         return """.pbot commands:
 ```
@@ -127,10 +131,10 @@ example usage:
   .pbot ping
 ```"""
 
-    def ping(self, args):
+    def ping(self, cmd_args):
         return {"message": "PONG!"}
 
-    def list_namespaces(self):
+    def list_namespaces(self, cmd_args):
         response = [
             "The following namespaces are registered with pbot: ",
             "_(For each of the following namespaces try `.namespace help`)_"
@@ -141,13 +145,13 @@ example usage:
         response.append("```")
         return '\n'.join(response)
 
-    def test_handler(self, arg_list):
-        if not arg_list:
+    def test_handler(self, cmd_args):
+        if not cmd_args:
             return {"message": F":red_circle: test requires a URL as an argument"}
 
         # the only expected argument is the url of the service, so ignore any others.  In addition, slack sends links
         # surrounded by angle brackets (<, >) if it recognizes a URL, so we need to extract the URL from that.
-        substring = SlackFormattedSubstring(arg_list[0])
+        substring = SlackFormattedSubstring(cmd_args[0])
         ns_url = substring.get_content_or_none() if substring.is_url_link() else substring.get_raw()
 
         if not validators.url(ns_url):
